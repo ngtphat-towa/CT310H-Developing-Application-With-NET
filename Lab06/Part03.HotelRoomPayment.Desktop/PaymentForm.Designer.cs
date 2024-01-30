1e1f28e383a22d560d914876bc2d8039ca8fae06
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-01-30 17:44:55 +0700

    Add Lab06 Part 3 Desktop

---
+++ Lab06/Part03.HotelRoomPayment.Desktop/PaymentForm.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Part03.HotelRoomPayment.Desktop
+{
+    partial class PaymentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbMain = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtContractNo = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.txtPayment = new System.Windows.Forms.TextBox();
+            this.cbRoomNo = new System.Windows.Forms.ComboBox();
+            this.dtpPaymentDate = new System.Windows.Forms.DateTimePicker();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbMain.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbMain
+            // 
+            this.tbMain.AutoSize = true;
+            this.tbMain.ColumnCount = 3;
+            this.tbMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tbMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tbMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tbMain.Controls.Add(this.label1, 1, 0);
+            this.tbMain.Controls.Add(this.tableLayoutPanel1, 1, 1);
+            this.tbMain.Controls.Add(this.tableLayoutPanel2, 1, 2);
+            this.tbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbMain.Location = new System.Drawing.Point(0, 0);
+            this.tbMain.Name = "tbMain";
+            this.tbMain.RowCount = 4;
+            this.tbMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tbMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.93575F));
+            this.tbMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.06426F));
+            this.tbMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tbMain.Size = new System.Drawing.Size(832, 373);
+            this.tbMain.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.label1.Size = new System.Drawing.Size(786, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Payment Form";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 8;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 4, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 4, 6);
+            this.tableLayoutPanel1.Controls.Add(this.txtContractNo, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtCustomerName, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtCustomerID, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtPayment, 2, 6);
+            this.tableLayoutPanel1.Controls.Add(this.cbRoomNo, 6, 4);
+            this.tableLayoutPanel1.Controls.Add(this.dtpPaymentDate, 6, 6);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(23, 53);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(786, 245);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Contract No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Customer Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 22);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "ID No.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 22);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Payment";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(403, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 22);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Room No.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(403, 183);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 22);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Payment Date";
+            // 
+            // txtContractNo
+            // 
+            this.txtContractNo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtContractNo.Enabled = false;
+            this.txtContractNo.Location = new System.Drawing.Point(164, 3);
+            this.txtContractNo.Name = "txtContractNo";
+            this.txtContractNo.Size = new System.Drawing.Size(213, 30);
+            this.txtContractNo.TabIndex = 6;
+            // 
+            // txtCustomerName
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtCustomerName, 5);
+            this.txtCustomerName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtCustomerName.Location = new System.Drawing.Point(164, 64);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(596, 30);
+            this.txtCustomerName.TabIndex = 7;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtCustomerID.Location = new System.Drawing.Point(164, 125);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(213, 30);
+            this.txtCustomerID.TabIndex = 8;
+            // 
+            // txtPayment
+            // 
+            this.txtPayment.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtPayment.Location = new System.Drawing.Point(164, 186);
+            this.txtPayment.Name = "txtPayment";
+            this.txtPayment.Size = new System.Drawing.Size(213, 30);
+            this.txtPayment.TabIndex = 9;
+            // 
+            // cbRoomNo
+            // 
+            this.cbRoomNo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cbRoomNo.FormattingEnabled = true;
+            this.cbRoomNo.Location = new System.Drawing.Point(547, 125);
+            this.cbRoomNo.Name = "cbRoomNo";
+            this.cbRoomNo.Size = new System.Drawing.Size(213, 30);
+            this.cbRoomNo.TabIndex = 10;
+            // 
+            // dtpPaymentDate
+            // 
+            this.dtpPaymentDate.CustomFormat = "dd/MM/yyy hh:mm:ss";
+            this.dtpPaymentDate.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dtpPaymentDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpPaymentDate.Location = new System.Drawing.Point(547, 186);
+            this.dtpPaymentDate.Name = "dtpPaymentDate";
+            this.dtpPaymentDate.Size = new System.Drawing.Size(213, 30);
+            this.dtpPaymentDate.TabIndex = 11;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.Controls.Add(this.btnExit, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnSave, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(20, 301);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(792, 48);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnExit.Location = new System.Drawing.Point(552, 3);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(25, 3, 25, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(215, 36);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSave.Location = new System.Drawing.Point(288, 3);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(25, 3, 25, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(214, 36);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAdd.Location = new System.Drawing.Point(25, 3);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(25, 3, 25, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(213, 36);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // PaymentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 373);
+            this.Controls.Add(this.tbMain);
+            this.Font = new System.Drawing.Font("Microsoft New Tai Lue", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.MaximumSize = new System.Drawing.Size(900, 550);
+            this.MinimumSize = new System.Drawing.Size(720, 360);
+            this.Name = "PaymentForm";
+            this.Text = "Room Payment";
+            this.tbMain.ResumeLayout(false);
+            this.tbMain.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tbMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtContractNo;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.TextBox txtPayment;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cbRoomNo;
+        private System.Windows.Forms.DateTimePicker dtpPaymentDate;
+    }
+}
+
---
+++ Lab06/Part03.HotelRoomPayment.Desktop/PaymentForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part03.HotelRoomPayment.Desktop
+{
+    public partial class PaymentForm : Form
+    {
+        public PaymentForm()
+        {
+            InitializeComponent();
+            PopulateRoomCombobox();
+            ResetMenuContext();
+        }
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["QLCBConnectionString"].ConnectionString;
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ResetMenuContext(true);
+            txtContractNo.Text = (GetCurrentMaxInvoiceId()+1).ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var invoiceId = Convert.ToInt32(txtContractNo.Text);
+            var guestName = txtCustomerName.Text;
+            var idNumber = txtCustomerID.Text;
+            var paymentAmount =Convert.ToDecimal(txtPayment.Text);
+            var roomId = Convert.ToInt32(cbRoomNo.SelectedValue);
+
+            SaveGuestInvoice(invoiceId, guestName, idNumber, paymentAmount, roomId);
+            ResetMenuContext();
+        }
+        public bool SaveGuestInvoice(int invoiceId, string guestName, string idNumber, decimal paymentAmount, int roomId)
+        {
+            // Get current date and generate invoice ID
+            DateTime currentDate = DateTime.Now;
+
+            // Build SQL insert statement
+            string sql = "INSERT INTO Guests (InvoiceID, GuestName, IDNumber, PaymentAmount, PaymentDate, RoomID) VALUES (@invoiceId, @guestName, @idNumber, @paymentAmount, @paymentDate, @roomId)";
+
+            // Use try-catch block for safe database interaction
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@invoiceId", invoiceId);
+                        command.Parameters.AddWithValue("@guestName", guestName);
+                        command.Parameters.AddWithValue("@idNumber", idNumber);
+                        command.Parameters.AddWithValue("@paymentAmount", paymentAmount);
+                        command.Parameters.AddWithValue("@paymentDate", currentDate);
+                        command.Parameters.AddWithValue("@roomId", roomId);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected == 1)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error saving guest invoice. No rows affected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving guest invoice: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void PopulateRoomCombobox()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter("SELECT RoomID, RoomName FROM dbo.Rooms", connection);
+
+                    // Create a DataTable to hold the results
+                    DataTable positionsTable = new DataTable();
+                    adapter.Fill(positionsTable);
+
+                    // Bind the combo box to the DataTable
+                    cbRoomNo.DataSource = positionsTable;
+                    cbRoomNo.DisplayMember = "RoomName";
+                    cbRoomNo.ValueMember = "RoomID";
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private int GetCurrentMaxInvoiceId()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    string query = "select MAX(InvoiceID) from dbo.Guests;";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    connection.Open();
+                    var result =   command.ExecuteScalar();
+                    connection.Close();
+                    return Convert.ToInt32(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+        }
+        private void ResetMenuContext(bool isAddNew = false)
+        {
+            // Clear the context menu
+            txtContractNo.Clear();
+            txtCustomerName.Clear();
+            cbRoomNo.SelectedIndex = -1;
+            txtPayment.Clear();
+
+            // Resset the default state
+            btnSave.Enabled = isAddNew;
+            btnAdd.Enabled = !isAddNew;
+            txtContractNo.Focus();
+        }
+    }
+}
