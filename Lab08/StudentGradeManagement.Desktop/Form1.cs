bbb911da6b22b6db6ababee2c6ae60bc798e7967
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-02-01 16:11:13 +0700

    Add init Lab08

---
+++ Lab08/StudentGradeManagement.Desktop/Form1.cs
@@ -0,0 +1,10 @@
+namespace StudentGradeManagement.Desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
