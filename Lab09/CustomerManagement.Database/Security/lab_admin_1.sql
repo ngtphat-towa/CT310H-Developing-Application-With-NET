4b748bc63e13d2c3730196a6d09478d65768f896
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-04-03 11:50:58 +0700

    Add Lab09 Database

---
+++ Lab09/CustomerManagement.Database/Security/lab_admin_1.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [lab_admin]
+    WITH PASSWORD = N'mhd2q{{bshbeoxc<qunpqulPmsFT7_&#$!~<cqxdkr1ourWU', SID = 0xB5E19C863423E340AA7A9010EFC64B6D, DEFAULT_LANGUAGE = [us_english];
+
---
+++ Lab09/CustomerManagement.Database/dbo/Tables/Customer.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [Username]     NVARCHAR (20)  NOT NULL,
+    [HashPassword] VARCHAR (50)   NOT NULL,
+    [FullName]     NVARCHAR (100) NOT NULL,
+    [DateOfBirth]  NVARCHAR (100) NULL,
+    [Gender]       VARCHAR (5)    NULL,
+    [Email]        NVARCHAR (50)  NULL,
+    [Income]       MONEY          NULL,
+    PRIMARY KEY CLUSTERED ([Username] ASC),
+    CONSTRAINT [CHK_Email] CHECK ([dbo].[checkEmail]([Email])=(1)),
+    CONSTRAINT [CHK_Income] CHECK ([Income]>(1000000) AND [Income]<(50000000))
+);
+
