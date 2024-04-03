8d721b2647daef228c2cfec71b878b8b1be6709d
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-04-03 12:40:35 +0700

    Add SP InsertCustomer

---
+++ Lab09/CustomerManagement.Database/dbo/Stored Procedures/InsertCustomer.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE dbo.InsertCustomer
+    @Username NVARCHAR(20),
+    @HashPassword VARCHAR(50),
+    @FullName NVARCHAR(100),
+    @DataOfBirth NVARCHAR(100),
+    @Gender VARCHAR(5),
+    @Email NVARCHAR(50),
+    @Income MONEY
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check if the email is valid before inserting
+    IF dbo.checkEmail(@Email) = 0
+    BEGIN
+        PRINT 'Invalid email address';
+        RETURN; 
+    END
+
+    IF dbo.isLegitDate(@DataOfBirth) = 0
+    BEGIN
+        PRINT 'Invalid birthday';
+        RETURN; 
+    END
+    INSERT INTO Customer (Username, HashPassword, FullName, DateOfBirth, Gender, Email, Income)
+    VALUES (@Username, @HashPassword, @FullName, @DataOfBirth, @Gender, @Email, @Income);
+    
+    PRINT 'Customer inserted successfully';
+END
